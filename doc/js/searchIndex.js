e636d8509e7c364873890c687a23c89a0565e8d2
Aitor Gil <aitorjaro11@hotmail.com>
2024-02-07 22:42:05 +0100

    Versión 2

---
+++ doc/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\obtenerCampeones\u0028\u0029",
+            "name": "obtenerCampeones",
+            "summary": "Obtiene\u0020la\u0020lista\u0020de\u0020campeones\u0020de\u0020League\u0020of\u0020Legends",
+            "url": "namespaces/default.html#function_obtenerCampeones"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
--- index.php
+++ index.php
@@ -21,8 +21,17 @@
 		<h2>LISTA DE CAMPEONES</h2>
 		<p class="centro">Aquí se detallan todos los campeones de League of Legends, incluyendo sus descripciones.</p>
 		<section class="campeones">
-			
-				<?php
+
+			<?php
+			/**
+			 * Obtiene la lista de campeones de League of Legends
+			 *
+			 * Esta función usa la API de Riot Games para obtener los datos de los campeones
+			 * en formato JSON y los convierte en un array.
+			 * @return array El array con los campeones
+			 */
+			function obtenerCampeones()
+			{
 
 				$lista_campeones = file_get_contents("https://ddragon.leagueoflegends.com/cdn/14.3.1/data/en_US/champion.json");
 
@@ -30,18 +39,21 @@
 
 				$campeones = $arrayCampeones->data;
 
-				foreach ($campeones as $campeon) {
-					echo "<article>";
+				return $campeones;
+			}
 
-					echo '<img src="https://ddragon.leagueoflegends.com/cdn/14.3.1/img/champion/' . $campeon->image->full . '">' . "<br/>";
-					echo "<b><p>" . $campeon->name . "\n" . "</p></b>";
-					echo $campeon->title;
+			foreach (obtenerCampeones() as $campeon) {
+				echo "<article>";
 
-					echo "</article>";
-				}
+				echo '<img src="https://ddragon.leagueoflegends.com/cdn/14.3.1/img/champion/' . $campeon->image->full . '">' . "<br/>";
+				echo "<b><p>" . $campeon->name . "\n" . "</p></b>";
+				echo $campeon->title;
 
-				?>
-			
+				echo "</article>";
+			}
+
+			?>
+
 		</section>
 	</section>
 </body>
